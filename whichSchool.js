692d49ef54b1e56d1f9bf2467a1dcea07a26a5de
Adam Kowalczuk <adam.kowalczuk1@gmail.com>
2023-01-16 10:34:13 -0800

    Exercise using conditional statements

---
+++ whichSchool.js
@@ -0,0 +1,10 @@
+const whichSchool = function (age) {
+  if (age < 13) {
+    return "Elementary School";
+  } else if (age >= 13 && age <= 18) {
+    return "Secondary School";
+  } else {
+    return "Lighthouse Labs";
+  }
+}
+
