afedad2ab7349443dbbdb321873b87bc45af6b6f
Adam Kowalczuk <adam.kowalczuk1@gmail.com>
2023-01-19 17:34:45 -0800

    More fun with functions and the modulo operator

---
+++ odd.js
@@ -0,0 +1,8 @@
+const isOdd = function (num) {
+  return (!(num % 2 === 0));
+}
+
+console.log("3 is odd: " + isOdd(3));
+console.log("8 is odd: " + isOdd(8));
+console.log("15 is odd: " + isOdd(15));
+console.log("109797597 is odd: " + isOdd(109797597));
