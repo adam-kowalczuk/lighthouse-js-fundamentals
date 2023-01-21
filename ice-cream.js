6c591cb9edaf4e947957a1f2fb2b1a70deddb399
Adam Kowalczuk <adam.kowalczuk1@gmail.com>
2023-01-21 12:23:13 -0800

    Accessing elements within an array

---
+++ ice-cream.js
@@ -0,0 +1,6 @@
+const iceCreamFlavours = ["chocolate", "vanilla", "cookies and cream", "rocky road", "strawberry"];
+iceCreamFlavours.push("root beer");
+console.log(iceCreamFlavours);
+console.log(iceCreamFlavours[0]);
+console.log(iceCreamFlavours[5]);
+console.log(iceCreamFlavours.length);
